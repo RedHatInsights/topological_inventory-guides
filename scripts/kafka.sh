2bf4010cc13e639f9c9f16ea92fa6e295d378dcf
Martin Slemr <mslemr@redhat.com>
2019-03-06 10:44:18 +0100

    Changed TP-Inv kafka topic.

--- scripts/kafka.sh
+++ scripts/kafka.sh
@@ -13,9 +13,9 @@ if [ "$operation" = "start" ]; then
 	#bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --topic topological_inventory-persister --partitions 500 --config max.message.bytes=10000000	
 	#bin/kafka-topics.sh --alter --zookeeper localhost:2181 --replication-factor 1 --topic topological_inventory-persister --partitions 500 --config max.message.bytes=10000000	
 elif [ "$operation" = "fast-forget" ]; then
-	bin/kafka-configs.sh --zookeeper localhost:2181 --alter --entity-type topics --entity-name topological_inventory-persister --add-config retention.ms=1000
+	bin/kafka-configs.sh --zookeeper localhost:2181 --alter --entity-type topics --entity-name platform.topological-inventory.persister --add-config retention.ms=1000
 elif [ "$operation" = "rm-fast-forget" ]; then
-	bin/kafka-configs.sh --zookeeper localhost:2181 --alter --entity-type topics --entity-name topological_inventory-persister --delete-config retention.ms	
+	bin/kafka-configs.sh --zookeeper localhost:2181 --alter --entity-type topics --entity-name platform.topological-inventory.persister --delete-config retention.ms	
 elif [ "$operation" = "stop" ]; then
 	bin/kafka-server-stop.sh
 	bin/zookeeper-server-stop.sh
@@ -24,7 +24,7 @@ else
 	echo "--- Operation:---"
 	echo "start -> starts server"
 	echo "stop -> stops server"
-	echo "fast-forget -> clears topic topological_inventory-persister"
-	echo "rm-fast-forget -> sets message remember time to default value for topic topological_inventory-persister"
+	echo "fast-forget -> (run with kafka started) clears topic 'platform.topological-inventory.persister' (forgets messages instantly)"
+	echo "rm-fast-forget -> (run with kafka started) stop clearing topic 'platform.topological-inventory.persister'"
 fi	
 
