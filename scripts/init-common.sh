bccdfebf5d82352b569b077b2fb7c39f36004d05
Libor Pichler <lpichler@redhat.com>
2019-04-05 18:11:58 +0200

    don't load rvm in init-common

--- scripts/init-common.sh
+++ scripts/init-common.sh
@@ -1,4 +1,5 @@
-source "$HOME/.rvm/scripts/rvm"
-rvm use $RVM_RUBY_VERSION_TP_INV
-rvm gemset use $RVM_GEMSET_NAME_TP_INV
-
+if [ -f "$HOME/.rvm/scripts/rvm" ]; then
+  source "$HOME/.rvm/scripts/rvm"
+  rvm use $RVM_RUBY_VERSION_TP_INV
+  rvm gemset use $RVM_GEMSET_NAME_TP_INV
+fi
