36dc42a9deaeafcd43dee88f1149b3ab9fb747a6
Martin Slemr <mslemr@redhat.com>
2019-05-28 12:38:29 +0200

    UI fake-identity as x-rh-identity header

--- scripts/services/ui.sh
+++ scripts/services/ui.sh
@@ -7,4 +7,4 @@ echo "Starting UI"
 echo "See https://ci.foo.redhat.com:1337/insights/settings/sources"
 
 cd ${TOPOLOGICAL_INVENTORY_UI_DIR}
-BASE_PATH=http://${SOURCES_HOST}:${SOURCES_PORT}/api npm run start
+FAKE_IDENTITY=${MY_GITHUB_NAME} BASE_PATH=http://${SOURCES_HOST}:${SOURCES_PORT}/api npm run start
