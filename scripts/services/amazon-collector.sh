22173df00be4bcfb733e24c7b3760c4a789030f8
Martin Slemr <mslemr@redhat.com>
2019-06-12 11:27:27 +0200

    [FIX] collector's metrics-port arg

--- scripts/services/amazon-collector.sh
+++ scripts/services/amazon-collector.sh
@@ -4,4 +4,4 @@ source config.sh
 source init-common.sh
 
 cd $AMAZON_DIR
-bin/amazon-collector --source=${AMAZON_SOURCE_UID} --access-key-id=${AMAZON_ACCESS_KEY_ID} --secret-access-key=${AMAZON_SECRET_ACCESS_KEY}
+bin/amazon-collector --source=${AMAZON_SOURCE_UID} --access-key-id=${AMAZON_ACCESS_KEY_ID} --secret-access-key=${AMAZON_SECRET_ACCESS_KEY} --metrics-port ${METRICS_PORT}
