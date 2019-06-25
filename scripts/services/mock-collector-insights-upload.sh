32d5381aa75ff6b064caec323516d23a2d6a7db8
Martin Slemr <mslemr@redhat.com>
2019-06-25 14:46:42 +0200

    mock collector insights upload tenant arg

--- scripts/services/mock-collector-insights-upload.sh
+++ scripts/services/mock-collector-insights-upload.sh
@@ -5,4 +5,5 @@ source init-common.sh
 
 cd $MOCK_SOURCE_DIR
 bin/mock-collector-insights-upload --source $MOCK_SOURCE_UID --config $MOCK_SOURCE_CONFIG --data $MOCK_SOURCE_DATA \
---insights-upload-api="http://localhost:8888" --insights-upload-base-path="api/ingress/v1/upload"
+--insights-upload-api="http://localhost:8888" --insights-upload-base-path="api/ingress/v1/upload" \
+--tenant=$MY_GITHUB_NAME
