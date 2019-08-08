783a6c663f81c05a770c0d17e96aa142af20c580
Martin Slemr <mslemr@redhat.com>
2019-08-08 13:43:37 +0200

    API's binding to public IP

--- scripts/services/sources-api.sh
+++ scripts/services/sources-api.sh
@@ -4,5 +4,4 @@ source config.sh
 source init-common.sh
 
 cd ${SOURCES_API_DIR}
-env PORT=${SOURCES_PORT} PATH_PREFIX="api" APP_NAME="sources" rails s
-
+env PORT=${SOURCES_PORT} PATH_PREFIX="api" APP_NAME="sources" rails s --binding 0.0.0.0
