4d8125fc8bcd19a6c837b0c49a72058f230f28c0
Martin Slemr <mslemr@redhat.com>
2020-01-29 14:10:51 +0100

    Removing ManageIQ org. from cloning

--- scripts/services/openshift-collector.sh
+++ scripts/services/openshift-collector.sh
@@ -7,7 +7,7 @@ cd $OPENSHIFT_DIR
 
 oc login -u $OPENSHIFT_USER -p $OPENSHIFT_PASSWORD $OPENSHIFT_HOST:$OPENSHIFT_PORT;
 export OPENSHIFT_TOKEN=$(oc sa get-token -n management-infra management-admin)
-echo $OPENSHIFT_TOKEN
+
 # Single Source
 ./bin/openshift-collector --source $OPENSHIFT_SOURCE_UID --scheme $OPENSHIFT_SCHEME --host $OPENSHIFT_HOST --port $OPENSHIFT_PORT --token $OPENSHIFT_TOKEN --path $OPENSHIFT_API_PATH --metrics-port $METRICS_PORT
 # Multi Source
