0bdd841b22997b94a31cbb25baec3e0537fdce54
Richard Oliveri <roliveri@redhat.com>
2020-04-16 18:31:19 -0400

    scripts/api/common.sh - Automate the detection of the supported base64 options.

--- scripts/api/common.sh
+++ scripts/api/common.sh
@@ -6,9 +6,15 @@ plain_rh_identity="{\"identity\":{\"account_number\":\"${ACCOUNT_NUMBER}\",\"use
 
 # base64 can split output to more lines, forbid it with one of following
 # depends on base64 version:
-export X_RH_IDENTITY=$(echo ${plain_rh_identity} | base64 --wrap=0)
-# export X_RH_IDENTITY=$(echo ${plain_rh_identity} | base64)
-# export X_RH_IDENTITY=$(echo ${plain_rh_identity} | base64 --break=0)
+if base64 --wrap=0 < /dev/null 2> /dev/null > /dev/null
+then
+    export X_RH_IDENTITY=$(echo ${plain_rh_identity} | base64 --wrap=0)
+elif base64 --break=0 < /dev/null 2> /dev/null > /dev/null
+then
+    export X_RH_IDENTITY=$(echo ${plain_rh_identity} | base64 --break=0)
+else
+    export X_RH_IDENTITY=$(echo ${plain_rh_identity} | base64)
+fi
 
 # Usage: topological_api_get <path>
 # Example: topological_api_get "source_types?filter[name]=mock"
