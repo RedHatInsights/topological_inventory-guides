092da45f8f844e8838cf9203cde27e2b77af528a
Martin Slemr <mslemr@redhat.com>
2020-08-05 10:31:42 +0200

    Better API Call Examples

--- scripts/api/examples.sh
+++ scripts/api/examples.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+#
+# There are examples of Topological API/Sources API calls
+# You can use file api/dev.sh (in gitignore)
 
 source "api/common.sh"
 #
@@ -42,35 +45,28 @@ source "api/common.sh"
 # sources_api_get "applications?filter[application_type_id][eq][]=1&filter[application_type_id][eq][]=3"
 # echo ""
 
+# EXAMPLES OF CREATING SOURCE WITH RELATIONS ---
+
+#sources_api_post "sources" "{\"uid\": \"1111\",\"name\":\"Azure 1\",\"source_type_id\":\"3\", \"availability_status\": \"available\"}"
+#sources_api_post "endpoints" "{\"source_id\":\"9\",\"default\":\"true\",\"path\":\"/\"}"
+#sources_api_post "authentications" "{\"resource_type\":\"Endpoint\",\"resource_id\":\"6\",\"authtype\":\"tenant_id_client_id_client_secret\",\"username\":\"redhat\",\"password\":\"redhat\",\"extra\":{\"azure\":{\"tenant_id\":\"12345\"}}}"
+#sources_api_post "applications" "{\"source_id\":\"9\",\"application_type_id\":\"4\",\"availability_status\":\"available\"}"
+
 # GRAPHQL -----------------------
 
 # sources_api_post "graphql" "{\"query\": \"{ sources { id, created_at, source_type_id, name, tenant, uid, updated_at applications { application_type_id, id }, endpoints { id, scheme, host, port, path } }}\"}"
 
+# SOURCE AVAILABILITY CHECK -----
+# sources_api_post "sources/4/check_availability"
 
 # CATALOG ORDERING --------------
-# Job template
+# Job template (ServiceOffering)
 # topological_api_post "service_offerings/2/order" "{\"service_parameters\":{\"username\":\"mslemr\",\"quest\":\"Test Topology\",\"airspeed\":50},\"provider_control_parameters\":{}}"
 
-# Workflow Job template
+# Workflow Job template (ServiceOffering)
 # topological_api_post "service_offerings/5/order" "{\"service_parameters\":{\"dev_null\":\"Yes\"},\"provider_control_parameters\":{}}"
 
-#topological_api_post "service_offerings/3/order" "{\"provider_control_parameters\":{}}"
+# Applied Inventories operation (before ordering - Approval operation)
+# topological_api_post "service_offerings/569/applied_inventories" "{}"
 
-#topological_api_post "service_offerings/569/applied_inventories" "{}"
-
-#sources_api_post "sources" "{\"uid\": \"1111\",\"name\":\"Azure 1\",\"source_type_id\":\"3\", \"availability_status\": \"available\"}"
-#sources_api_post "sources" "{\"uid\": \"2222\",\"name\":\"Azure 2\",\"source_type_id\":\"3\", \"availability_status\": \"available\"}"
-#sources_api_post "sources" "{\"uid\": \"3333\",\"name\":\"Azure 3\",\"source_type_id\":\"3\", \"availability_status\": \"available\"}"
-#sources_api_post "sources" "{\"uid\": \"4444\",\"name\":\"Azure 4\",\"source_type_id\":\"3\", \"availability_status\": \"available\"}"
-#sources_api_post "endpoints" "{\"source_id\":\"9\",\"default\":\"true\",\"path\":\"/\"}"
-#sources_api_post "endpoints" "{\"source_id\":\"10\",\"default\":\"true\",\"path\":\"/\"}"
-#sources_api_post "endpoints" "{\"source_id\":\"11\",\"default\":\"true\",\"path\":\"/\"}"
-#sources_api_post "authentications" "{\"resource_type\":\"Endpoint\",\"resource_id\":\"6\",\"authtype\":\"tenant_id_client_id_client_secret\",\"username\":\"redhat\",\"password\":\"redhat\",\"extra\":{\"azure\":{\"tenant_id\":\"12345\"}}}"
-#sources_api_post "authentications" "{\"resource_type\":\"Endpoint\",\"resource_id\":\"7\",\"authtype\":\"tenant_id_client_id_client_secret\",\"username\":\"redhat\",\"password\":\"redhat\",\"extra\":{\"azure\":{\"tenant_id\":\"12345\"}}}"
-#sources_api_post "authentications" "{\"resource_type\":\"Endpoint\",\"resource_id\":\"8\",\"authtype\":\"tenant_id_client_id_client_secret\",\"username\":\"redhat\",\"password\":\"redhat\",\"extra\":{\"azure\":{\"tenant_id\":\"12345\"}}}"
-#sources_api_post "authentications" "{\"resource_type\":\"Endpoint\",\"resource_id\":\"9\",\"authtype\":\"tenant_id_client_id_client_secret\",\"username\":\"redhat\",\"password\":\"redhat\",\"extra\":{\"azure\":{\"tenant_id\":\"12345\"}}}"
-#sources_api_post "applications" "{\"source_id\":\"8\",\"application_type_id\":\"4\",\"availability_status\":\"available\"}"
-#sources_api_post "applications" "{\"source_id\":\"9\",\"application_type_id\":\"4\",\"availability_status\":\"available\"}"
-#sources_api_post "applications" "{\"source_id\":\"10\",\"application_type_id\":\"4\",\"availability_status\":\"available\"}"
-sources_api_post "applications" "{\"source_id\":\"11\",\"application_type_id\":\"4\",\"availability_status\":\"available\"}"
 echo ""
